02fc87ebba0a0e74933fa4b02ad6f02bc22b19d6
Denis Timakov <timakden88@gmail.com>
2021-08-26 00:00:23 +0300

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     idea
     id("com.github.ben-manes.versions") version "0.39.0"
-    id("org.springframework.boot") version "2.5.3"
+    id("org.springframework.boot") version "2.5.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.21"
-    kotlin("plugin.spring") version "1.5.21"
+    kotlin("jvm") version "1.5.30"
+    kotlin("plugin.spring") version "1.5.30"
 }
 
 group = "ru.timakden"
@@ -29,7 +29,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
     implementation("org.camunda.bpm.springboot:camunda-bpm-spring-boot-starter-webapp:7.15.0")
-    implementation("com.sun.xml.bind:jaxb-impl:3.0.1")
+    implementation("com.sun.xml.bind:jaxb-impl:3.0.2")
     implementation("com.h2database:h2")
 }
 
