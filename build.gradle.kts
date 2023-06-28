54ae18cea991390094cded72f2c5b067a04c7fb4
Denis Timakov <timakden88@gmail.com>
2023-06-28 19:50:53 +0300

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     idea
-    id("com.github.ben-manes.versions") version "0.46.0"
-    id("org.springframework.boot") version "3.0.6"
+    id("com.github.ben-manes.versions") version "0.47.0"
+    id("org.springframework.boot") version "3.1.1"
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.spring") version "1.8.21"
@@ -18,7 +18,7 @@ repositories {
 
 dependencyManagement {
     imports {
-        mavenBom("org.springframework.boot:spring-boot-dependencies:3.0.6")
+        mavenBom("org.springframework.boot:spring-boot-dependencies:3.1.1")
     }
 }
 
@@ -26,19 +26,19 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
     implementation("org.camunda.bpm.springboot:camunda-bpm-spring-boot-starter-webapp:7.19.0")
-    implementation("com.sun.xml.bind:jaxb-impl:4.0.2")
+    implementation("com.sun.xml.bind:jaxb-impl:4.0.3")
     implementation("com.h2database:h2:2.1.214")
 }
 
 kotlin {
-    jvmToolchain(19)
+    jvmToolchain(17)
 }
 
 tasks {
     compileKotlin {
         compilerOptions {
             freeCompilerArgs.add("-Xjsr305=strict")
-            jvmTarget.set(JvmTarget.JVM_19)
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
 }
