78eb6c7e28855a86dfae85f96dd4a8d9df8e7da1
Denis Timakov <timakden88@gmail.com>
2024-03-08 18:43:32 +0300

    Update versions of spring boot and kotlin in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.js.translate.context.Namer.kotlin
 
 plugins {
     idea
     id("com.github.ben-manes.versions") version "0.51.0"
-    id("org.springframework.boot") version "3.2.2"
+    id("org.springframework.boot") version "3.2.3"
     id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.spring") version "1.9.22"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
 }
 
 group = "ru.timakden"
@@ -18,7 +19,7 @@ repositories {
 
 dependencyManagement {
     imports {
-        mavenBom("org.springframework.boot:spring-boot-dependencies:3.2.2")
+        mavenBom("org.springframework.boot:spring-boot-dependencies:3.2.3")
     }
 }
 
@@ -26,8 +27,8 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
     implementation("org.camunda.bpm.springboot:camunda-bpm-spring-boot-starter-webapp:7.20.0")
-    implementation("com.sun.xml.bind:jaxb-impl:4.0.4")
-    implementation("com.h2database:h2:2.2.224")
+    implementation("com.sun.xml.bind:jaxb-impl")
+    implementation("com.h2database:h2")
 }
 
 kotlin {
@@ -40,6 +41,9 @@ tasks {
             freeCompilerArgs.add("-Xjsr305=strict")
             jvmTarget.set(JvmTarget.JVM_21)
         }
+    }
+    wrapper {
+        gradleVersion = "8.6"
     }
 }
 
