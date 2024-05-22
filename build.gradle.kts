b4ebe4d67804ab3eaf28a6cd3d94b1818b494556
Denis Timakov <timakden88@gmail.com>
2024-05-22 10:12:22 +0300

    Upgrade Kotlin version to 2.0.0
    In the build.gradle.kts file, the Kotlin version for jvm and plugin.spring was upgraded to 2.0.0 from 1.9.24, to make use of the latest features and improvements provided by the new version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
     id("com.github.ben-manes.versions") version "0.51.0"
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.5"
-    kotlin("jvm") version "1.9.24"
-    kotlin("plugin.spring") version "1.9.24"
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.spring") version "2.0.0"
 }
 
 group = "ru.timakden"
