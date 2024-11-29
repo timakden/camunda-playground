e9dd145977d784dd1c56979be6db601c97f9694c
Denis Timakov <timakden@icloud.com>
2024-11-29 09:47:58 +0300

    Update Kotlin to version 2.1.0
    Upgraded Kotlin and Kotlin Spring plugin from version 2.0.21 to 2.1.0 in build.gradle.kts. This update may include improvements and bug fixes, ensuring better compatibility and performance in the project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
     id("com.github.ben-manes.versions") version "0.51.0"
     id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.6"
-    kotlin("jvm") version "2.0.21"
-    kotlin("plugin.spring") version "2.0.21"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.spring") version "2.1.0"
 }
 
 group = "ru.timakden"
